15175c90b5e44282dbd83541161637c7a5ba1bdb
sjbeskur <sam.beskur@gmail.com>
2022-02-14 18:46:51 -0700

    reorg

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate camera_models;
-
 use camera_models::cameras;
 use camera_models::cameras::{PinholeCamera, Projection};
 
@@ -14,7 +12,29 @@ fn main() {
     //println!("{}", cam.get_intrinsics());
 
     let mut mtx = Matrix3x4::new(
-        1.0, 0.0, 0.0, 0.069266, 0.0, 1.0, 0.0, -0.129384, 1.0, 0.0, 1.0, 0.026577,
+        1.0, 0.0, 0.0, 0.069266, 
+        0.0, 1.0, 0.0, -0.129384, 
+        1.0, 0.0, 1.0, 0.026577,
+    );
+
+    let cam0 = cameras::Camera::new(mtx);
+
+    let pinhole_cameras: Vec<&dyn PinholeCamera> = vec![&cam, &cam0];
+    for p in pinhole_cameras {
+        println!("{}", p);
+    }
+
+    cam0.project();
+}
+
+
+
+#[test]
+fn matrix_ops(){
+    let mut mtx = Matrix3x4::new(
+        1.0, 0.0, 0.0, 0.069266, 
+        0.0, 1.0, 0.0, -0.129384, 
+        1.0, 0.0, 1.0, 0.026577,
     );
 
     let (r,c) = mtx.iamax_full();
@@ -30,12 +50,4 @@ fn main() {
     println!("{}",mtx);
     println!("{}",mtx.row(0) * 100.0);
 
-    let cam0 = cameras::Camera::new(mtx);
-
-    let pinhole_cameras: Vec<&dyn PinholeCamera> = vec![&cam, &cam0];
-    for p in pinhole_cameras {
-        println!("{}", p);
-    }
-
-    cam0.project();
 }
