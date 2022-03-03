4bc1e8ec624eac202ecd5f712efc67dfdbae4380
sam beskur <samuel.j.beskur@lmco.com>
2022-03-02 22:41:08 -0700

    hacks

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ fn main() {
     let cam = Camera::default();
 
 
-    let mut Kmtx = Matrix3::new(
+    let k_mtx = Matrix3::new(
         31818.1818, 0.0, 1295.5,
         0.0, 31818.1818, 971.5,
         0.0, 0.0, 1.0
     );
 
-    let mut T1 = Matrix3x4::new(
+    let mut _t1 = Matrix3x4::new(
         1.0, 0.0, 0.0, 0.069266, 
         0.0, 1.0, 0.0, -0.129384, 
         1.0, 0.0, 1.0, 0.026577,
     );
 
-    let cam0 =Camera::new(Kmtx);
+    let cam0 =Camera::new(k_mtx);
 
     let pinhole_cameras: Vec<&dyn PinholeCamera> = vec![&cam, &cam0];
     for p in pinhole_cameras {
