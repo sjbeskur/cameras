27ca093ab0996263c1bcef5ac1c2d8dadb74d5b6
sjbeskur <sam.beskur@gmail.com>
2023-10-06 23:45:57 -0600

    projection experiements

--- src/cameras/perspective_projection.rs
+++ src/cameras/perspective_projection.rs
@@ -7,7 +7,7 @@ pub trait PerspectiveProjection {
     fn unproject(&self, uv: &Point2<f32>) -> Point3<f32>;
 }
 pub trait LinearizeProjection {
-    fn project_linearize(&self, world_point: &Point3<f32>, jacobian: &mut SMatrix<f32, 2,3>) -> Point2<f32>;
+    fn project_linearize(&self, world_point: &SMatrix<f32, 3,1>, jacobian: &mut SMatrix<f32, 2,3>) -> Point2<f32>;
     //fn unproject(&self, uv: &Point2<f32>) -> Point3<f32>;
 }
 
@@ -39,7 +39,7 @@ type Jacobian2x3 = SMatrix<f32, 2, 3>;
 type Matrix2x1 = SMatrix<f32, 2,1>;
 impl LinearizeProjection for PinholeCamera {
 
-    fn project_linearize(&self, world_point: &Point3<f32>, jacobian: &mut SMatrix<f32, 2,3>) -> Point2<f32>{
+    fn project_linearize(&self, world_point: &SMatrix<f32, 3,1>, jacobian: &mut SMatrix<f32, 2,3>) -> Point2<f32>{
         let rk = self.k * world_point;
         let s: f32 = 1.0 / rk.z;
         let uv = Matrix2x1::new(rk.x * s, rk.y * s);
